ec825e707210163f790d5cd5e8e8c21c0f2f22ac
JiangNanYu639 <79446547+JiangNanYu639@users.noreply.github.com>
2021-02-22 17:24:50 +0800

    Add files via upload

---
+++ HM0222.py
@@ -0,0 +1,11 @@
+# a brief self-introduction
+
+gender = 'male'
+
+height = 1.78
+
+name = 'JiangNanYu'
+
+address = 'HunNan District, ShenYang, Liaoning Province, China'
+
+print('I am %s with a height of %.2fm. My name is %s and I live at %s.'%(gender,height,name,address))
